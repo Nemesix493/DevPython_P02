8c4bcc13fa3c04e74b61f59351542bda01de5d5b
Kevin Verquerre <verquerre.kevin@gmail.com>
2021-10-22 15:53:10 +0200

    ajouts de deux fonctions : - get_all_category_data qui recupere le lien et le noms de toutes les categorie du site - brows_category_pages qui recupere les lien des autres pages de la categorie si elle est paginer

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from time import sleep
+import csv
 
 
 
@@ -20,7 +21,7 @@ def get_data_from_product_page(url):
     soup = BeautifulSoup(response.content, "html.parser")
     product = soup.find('div', class_="content")
     product_data['image_url'] = urljoin(url, product.find('img')['src'])
-    product_data['product_description'] = product.find('p', class_="").text
+    product_data['product_description'] = product.find('p', class_="").text.replace('\n', '')
     table_lines = product.find('table').find_all('tr')
     product_data['universal_product_code'] = table_lines[0].find('td').text
     product_data['price_excluding_tax'] = table_lines[2].find('td').text.replace('£', '')
@@ -36,13 +37,46 @@ def get_data_from_product_page(url):
     product_data['category'] = soup.find('ul', class_="breadcrumb").find_all('li')[2].text.replace('\n', '')
     return product_data
 
+def get_all_categorys_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    links = soup.find('ul', class_="nav-list").find('ul').find_all('a')
+    categorys_data = []
+    for link in links:
+        categorys_data.append({
+            'link': urljoin(url,link['href']),
+            'name': link.text.replace(' ', '').replace('\n', ''),
+        })
+    return categorys_data
 
+def brows_category_pages(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    books_number = int(soup.find('form', class_="form-horizontal").find('strong').text)
+    pages_number = books_number//20
+    if books_number%20 > 0 :
+        pages_number += 1
+    links = [url]
+    for i in range(pages_number-1):
+        links.append(urljoin(url,"page-" + str(i+2) + ".html"))
+    return links
 
 
 base_url = "http://books.toscrape.com/"
-all_urls = get_all_url_product(base_url)
+
+categorys = get_all_categorys_data(base_url)
+print(brows_category_pages(categorys[3]['link']))
+
+"""all_urls = get_all_url_product(base_url)
 collected_data = []
 for url in all_urls:
     collected_data.append(get_data_from_product_page(url))
-    sleep(1.5)
-print(collected_data[18])
+    #sleep(1.5)
+
+with open('datas.csv', 'w', newline='') as fichier_csv:
+   # Créer un objet writer (écriture) avec ce fichier
+   writer = csv.writer(fichier_csv, delimiter=',')
+   writer.writerow(list(collected_data[0].keys()))
+   for data in collected_data:
+      writer.writerow(list(data.values()))
+"""
