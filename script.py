80663036dd3adf998eea40f400ebf9d1b136d5c0
Kevin Verquerre <verquerre.kevin@gmail.com>
2021-10-30 20:59:23 +0200

    ajout des fonctions : - save_as_csv qui enregistre les données dans un csv - main qui d'écris l'execution du script

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from time import sleep
 import csv
-
+from os.path import join
+from os import getcwd, mkdir
+from pathlib import Path
 
 
 def get_all_url_product(url):
@@ -15,13 +16,15 @@ def get_all_url_product(url):
         urls.append(urljoin(url, product.find("a")['href']))
     return urls
 
+
 def get_data_from_product_page(url):
     response = requests.get(url)
-    product_data = {'product_page_url' : url}
+    product_data = {'product_page_url': url}
     soup = BeautifulSoup(response.content, "html.parser")
     product = soup.find('div', class_="content")
     product_data['image_url'] = urljoin(url, product.find('img')['src'])
-    product_data['product_description'] = product.find('p', class_="").text.replace('\n', '')
+    if product.find('p', class_="") != None:
+        product_data['product_description'] = product.find('p', class_="").text.replace('\n', '')
     table_lines = product.find('table').find_all('tr')
     product_data['universal_product_code'] = table_lines[0].find('td').text
     product_data['price_excluding_tax'] = table_lines[2].find('td').text.replace('£', '')
@@ -30,12 +33,13 @@ def get_data_from_product_page(url):
     product_data['title'] = product.find('h1').text
     availability = product.find('p', class_="availability")
     if 'instock' in availability['class']:
-        product_data['number_available'] = availability.text.replace(' ','').replace('\n\n\nInstock(', '').replace('available)\n\n','')
+        product_data['number_available'] = availability.text.replace(' ', '').replace('\n\n\nInstock(', '').replace('available)\n\n', '')
     else:
         product_data['number_available'] = "0"
     product_data['review_rating'] = product.find('p', class_='star-rating')['class'][1]
     product_data['category'] = soup.find('ul', class_="breadcrumb").find_all('li')[2].text.replace('\n', '')
     return product_data
+
 
 def get_all_categorys_data(url):
     response = requests.get(url)
@@ -44,39 +48,59 @@ def get_all_categorys_data(url):
     categorys_data = []
     for link in links:
         categorys_data.append({
-            'link': urljoin(url,link['href']),
+            'link': urljoin(url, link['href']),
             'name': link.text.replace(' ', '').replace('\n', ''),
         })
     return categorys_data
+
 
 def brows_category_pages(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     books_number = int(soup.find('form', class_="form-horizontal").find('strong').text)
     pages_number = books_number//20
-    if books_number%20 > 0 :
+    if books_number % 20 > 0:
         pages_number += 1
     links = [url]
     for i in range(pages_number-1):
-        links.append(urljoin(url,"page-" + str(i+2) + ".html"))
+        links.append(urljoin(url, "page-" + str(i+2) + ".html"))
     return links
 
 
-base_url = "http://books.toscrape.com/"
+def save_as_csv(dir, name, datas):
+    path = join(getcwd(), dir)
+    file_path = Path(path)
+    if not file_path.exists():
+        mkdir(path)
+    with open(join(getcwd(), dir, name + '.csv'), 'w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.DictWriter(csv_file, [
+            'product_page_url',
+            'image_url', 'product_description',
+            'universal_product_code',
+            'price_excluding_tax',
+            'price_including_tax',
+            'review_rating',
+            'title',
+            'number_available',
+            'category'], delimiter=',')
+        writer.writeheader()
+        writer.writerows(datas)
 
-categorys = get_all_categorys_data(base_url)
-print(brows_category_pages(categorys[3]['link']))
 
-"""all_urls = get_all_url_product(base_url)
-collected_data = []
-for url in all_urls:
-    collected_data.append(get_data_from_product_page(url))
-    #sleep(1.5)
+def main():
+    base_url = "http://books.toscrape.com/"
+    categorys = get_all_categorys_data(base_url)
+    for category in categorys:
+        category['links'] = brows_category_pages(category['link'])
+        category['books_link'] = []
+        for link in category['links']:
+            category['books_link'] = [*category['books_link'], *get_all_url_product(link)]
+        category['books_datas'] = []
+        for link in category['books_link']:
+            category['books_datas'].append(get_data_from_product_page(link))
+        save_as_csv('..\\collected_datas\\csv', category['name'], category['books_datas'])
+        print(category['name'] + ': ok !')
 
-with open('datas.csv', 'w', newline='') as fichier_csv:
-   # Créer un objet writer (écriture) avec ce fichier
-   writer = csv.writer(fichier_csv, delimiter=',')
-   writer.writerow(list(collected_data[0].keys()))
-   for data in collected_data:
-      writer.writerow(list(data.values()))
-"""
+
+if __name__ == '__main__':
+    main()
